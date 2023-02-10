2ee44f236d2bd070cad52079940251edb0600028
EDGAR ROMARIO DE PAULA DOS SANTOS <04339021164@ciops.local>
2023-02-10 03:03:12 -0400

    Adjusted the columns to four and renamed some fields

--- src/components/Cvgen.jsx
+++ src/components/Cvgen.jsx
@@ -1,26 +1,34 @@
 import jsPDF from 'jspdf'
 
-export function Cvgen () {
+export function Cvgen({ name, lastName, age, maritalStatus, address, district, city, stt, zipCode, tel, email, linkedin, description }) {
 
-    const doc = new jsPDF({ orientation: 'p', unit: 'mm', format: 'a4', putOnlyUsedFonts: true })
+    const genPDF = () => {
+        const doc = new jsPDF({ orientation: 'p', unit: 'mm', format: 'a4', putOnlyUsedFonts: true })
 
-        doc.setFontSize(30)
-        doc.text(6, 16, `${name} ${lastName}`)
-        doc.setFontSize(20)
-        doc.text(6, 23, `${page} anos`)
-        doc.text(6, 30, `${maritalStatus}`)
-        doc.setDrawColor(255, 0, 0)
-        doc.setLineWidth(1.5)
-        doc.line(75, 300, 75, 58)
+
+        doc.setDrawColor(255, 0, 0) // red
+        doc.setLineWidth(1.5) // line thickness
+        doc.line(0, 50, 1000, 50) // (x1, y1, x2, y2) coordinates
+
+        doc.setFontSize(24)
+        doc.text(6, 15, `${name} ${lastName}`)
+        doc.setFontSize(18)
+        doc.text(8, 24, age ? `anos - ${maritalStatus}` : '')
         doc.setFontSize(10)
-        
         //full address
-        doc.text(8, 40, `Endereço: ${address}. ${district}. ${city}. ${stt}.`)
-
-        doc.text(8, 45, `Telefone: ${tel}`)
-        doc.text(8, 50, `Email: ${email}`)
-        doc.text(8, 55, linkedin ? `LinkedIn: ${linkedin}` : '')
-
+        doc.text(8, 30, `Endereço: ${address}. ${district}. ${city} - ${stt}.`)
+        doc.text(8, 35, `CEP: ${zipCode} - Telefone: ${tel}`)
+        doc.text(8, 40, `Email: ${email}`)
+        doc.text(8, 45, linkedin ? `LinkedIn: ${linkedin}` : '')
+        doc.setFontSize(18)
+        doc.text(12, 62, 'Objetivos')
+        doc.setFontSize(10)
+        let splitText = doc.splitTextToSize(description, 180)
+        doc.text(splitText, 15, 70, {maxWidth: 180, align: "justify"} )
         doc.save('cv.pdf')
-        return
     }
+
+    return (
+        <button type='submit' onClick={genPDF} className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center'>Gerar PDF</button>
+    )
+}
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,91 @@
-import { Fields } from "../components/Fields"
+import { Link } from "react-router-dom"
+import { useState } from 'react'
+import { Cvgen } from "../components/Cvgen"
 
 export default function Profile() {
 
+    const [name, setName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [maritalStatus, setMaritalStatus] = useState('')
+    const [age, setAge] = useState('')
+    const [address, setAddress] = useState('')
+    const [district, setDistrict] = useState('')
+    const [city, setCity] = useState('')
+    const [stt, setStt] = useState('')
+    const [zipCode, setZipCode] = useState('')
+    const [tel, setTel] = useState('')
+    const [email, setEmail] = useState('')
+    const [linkedin, setLinkedin] = useState('')
+    const [description, setDescription] = useState('')
+    
+    const handleSubmit = e => e.preventDefault()
 
-    return <><Fields /></>
+    return (<div className='p-5 max-w-fit mx-auto px-10 bg-white rounded-3xl shadow-xl flex items-center space-x-4'>
+        <form onSubmit={handleSubmit}>
+        <div>
+            <h1 className='font-bold p-4 text-4xl text-gray-900'>Dados pessoais</h1>
+            <div className='p-5 grid gap-6 mb-6 md:grid-cols-4'>
+                <div>
+                    <label htmlFor='name' className='block mb-2 text-sm font-medium text-gray-900'>Nome</label>
+                    <input onChange={e => setName(e.target.value)} value={name} type='text' id='name' className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5' placeholder='João' required />
+                </div>
+                <div>
+                    <label htmlFor='lastname' className='block mb-2 text-sm font-medium text-gray-900'>Sobrenome</label>
+                    <input onChange={e => setLastName(e.target.value)} value={lastName} type='text' id='lastname' className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5' placeholder='Silva' required />
+                </div>
+                <div>
+                    <label htmlFor='maritalstatus' className='block mb-2 text-sm font-medium text-gray-900'>Estado Civil</label>
+                    <input onChange={e => setMaritalStatus(e.target.value)} value={maritalStatus} type='text' id='maritalstatus' className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5' placeholder='Casado' required />
+                </div>
+                <div>
+                    <label htmlFor='age' className='block mb-2 text-sm font-medium text-gray-900'>Idade</label>
+                    <input onChange={e => setAge(e.target.value)} value={age} type='number' id='age' className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5' placeholder='31' required />
+                </div>
+                <div>
+                    <label htmlFor='address' className='block mb-2 text-sm font-medium text-gray-900'>Endereço</label>
+                    <input onChange={e => setAddress(e.target.value)} value={address} type='text' id='address' className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5' placeholder='Rua ABC, 1234' required />
+                </div>
+                <div>
+                    <label htmlFor='district' className='block mb-2 text-sm font-medium text-gray-900'>Bairro</label>
+                    <input onChange={e => setDistrict(e.target.value)} value={district} type='text' id='district' className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5' placeholder='Tijuca' required />
+                </div>
+                <div>
+                    <label htmlFor='city' className='block mb-2 text-sm font-medium text-gray-900'>Cidade</label>
+                    <input onChange={e => setCity(e.target.value)} value={city} type='text' id='city' className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5' placeholder='Rio de Janeiro' required />
+                </div>
+                <div>
+                    <label htmlFor='state' className='block mb-2 text-sm font-medium text-gray-900'>Estado</label>
+                    <input onChange={e => setStt(e.target.value)} value={stt} type='text' id='state' className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5' placeholder='Rio de Janeiro' required />
+                </div>
+                <div>
+                    <label htmlFor='zipcode' className='block mb-2 text-sm font-medium text-gray-900'>CEP</label>
+                    <input onChange={e => setZipCode(e.target.value)} value={zipCode} type='text' id='zipcode' className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5' placeholder='00000-000' pattern='[(0-9)]{5}-[0-9]{3}' required />
+                </div>
+                <div>
+                    <label htmlFor='tel' className='block mb-2 text-sm font-medium text-gray-900'>Telefone</label>
+                    <input onChange={e => setTel(e.target.value)} value={tel} type='text' id='tel' className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5' placeholder='(00) 00000-0000' pattern='[(0-9)]{2} [0-9]{5}-[0-9]{4}' required />
+                </div>
+                <div>
+                    <label htmlFor='linkedin' className='block mb-2 text-sm font-medium text-gray-900'>LinkedIn</label>
+                    <input onChange={e => setLinkedin(e.target.value)} value={linkedin} type='url' id='linkedin' className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5' placeholder='linkedin.com/in/joaosilva' required />
+                </div>
+                <div>
+                    <label htmlFor='email' className='block mb-2 text-sm font-medium text-gray-900'>Email</label>
+                    <input onChange={e => setEmail(e.target.value)} value={email} type='email' id='email' className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5' placeholder='joaosilva@dominio.com' required />
+                </div>
+            </div>
+            <div className='mb-6'>
+                <label htmlFor='profile' className='block mb-2 text-sm font-medium text-gray-900'>Objetivos</label>
+                <textarea id='profile' onChange={e => setDescription(e.target.value)} value={description} className='resize-y bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5' placeholder='Descrição de perfil' />
+            </div>
+        </div>
+            <div className='p-2 grid gap-6 md:grid-cols-2'>
+                <button className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center'><Link to='/'>Voltar</Link></button>
+                <Cvgen name={name} lastName={lastName} maritalStatus={maritalStatus} age={age} address={address}
+                    district={district} city={city} stt={stt} zipCode={zipCode} tel={tel} linkedin={linkedin} email={email}
+                    description={description} />
+            </div>
+        </form>
+    </div>
+    )
 }
